49c9449020d36f74406e92e9217def77aef74395
GarrenBYU <mei21004@byui.edu>
2025-05-03 00:35:36 -0600

    Got rid of the intro

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,7 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
         List<int> numbers;
         numbers = new List<int>();
         string newNumber;
