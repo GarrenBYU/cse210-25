c89e328492b7a2571d346870691b8d2e75c5bf7e
GarrenBYU <mei21004@byui.edu>
2025-05-03 00:03:09 -0600

    excersize2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,38 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string number = Console.ReadLine();
+        int grade = int.Parse(number);
+        string letterGrade;
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"You're grade is {letterGrade}");
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congrats you passed the class. You got your next class for sure!");
+        }
+        else{
+            Console.WriteLine("I am afraid you didnt make the cut. You are going to need to retake this course. You got this next time.");
+        }
     }
 }
