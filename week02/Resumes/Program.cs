7fe1e6d94f63c62ad0f027ad951e0031e35f5068
GarrenBYU <mei21004@byui.edu>
2025-05-03 01:10:12 -0600

    Step 5 done

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -7,11 +7,15 @@ class Program
         Job job1 = new Job();
         job1._jobTitle = "Software Engineer";
         job1._company = "nCino";
+        job1._startYear = 2020;
+        job1._endYear = 2022;
         Job job2 = new Job();
         job2._jobTitle = "Intern";
         job2._company = "Apple";
-        Console.WriteLine(job1._company);
-        Console.WriteLine(job2._company);
+        job2._startYear = 2022;
+        job2._endYear = 2025;
+        job1.Display();
+        job2.Display();
 
     }
 }
