3ccd042a617c1444cccdafa026ac16d389c4eb40
GarrenBYU <mei21004@byui.edu>
2025-05-03 00:36:27 -0600

    dropped the intro

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,7 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
         string name;
         string number;
         int square;
