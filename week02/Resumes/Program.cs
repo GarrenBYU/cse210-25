690bce45ab242d4a6808647fbb8ec0f030bcb99c
GarrenBYU <mei21004@byui.edu>
2025-05-03 01:03:08 -0600

    Got to step 4

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "nCino";
+        Job job2 = new Job();
+        job2._jobTitle = "Intern";
+        job2._company = "Apple";
+        Console.WriteLine(job1._company);
+        Console.WriteLine(job2._company);
+
     }
 }
