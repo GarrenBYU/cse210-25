01775b84f14f4370cf771250a620b84b84d45c07
GarrenBYU <mei21004@byui.edu>
2025-05-31 10:43:49 -0600

    made the MathAssignment object

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -6,5 +6,9 @@ class Program
     {
         Assignment a1 = new Assignment("Samuel Bennett", "Multiplication");
         Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
     }
 }
