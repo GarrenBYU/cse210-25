fc45299ad12543f80c22cc398696d70519ed8749
GarrenBYU <mei21004@byui.edu>
2025-05-31 09:17:01 -0600

    First oder made in program

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        Address address1 = new Address("969 E Citadel Ln", "Saratoga Springs", "UT", "USA");
+        Customer customer1 = new Customer("Garren Meisman", address1);
+        Product prodcut1 = new Product("Picture Frame", 56287, 5, 8);
+        Product product2 = new Product("Picture", 951257, 12, 8);
+        Order order1 = new Order(customer1);
+        order1.PackingLabel();
+        order1.ShippingLabel();
     }
 }
