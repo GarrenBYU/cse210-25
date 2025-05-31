b2b18e80c49129394a5551c9f2d8346d0481a1d8
GarrenBYU <mei21004@byui.edu>
2025-05-31 00:38:09 -0600

    making the methods and the constructors for each class

--- week04/YouTubeVideos/Comment.cs
+++ week04/YouTubeVideos/Comment.cs
@@ -1,7 +1,18 @@
 using System;
 
-public class Comments
+public class Comment
 {
     public string _name; 
     public string _text; 
+
+        public Comment(string name, string text)
+        {
+            _text = text;
+            _name = name;
+        }
+
+    public void Display()
+    {
+        Console.WriteLine($"{_text} \n-{_name} ");
+    }
 }
--- week04/YouTubeVideos/Video.cs
+++ week04/YouTubeVideos/Video.cs
@@ -5,4 +5,19 @@ public class Video
     public string _title;
     public string _author;
     public int _seconds;
+    public List<Comment> _comments = new List<Comment>();
+
+    public Video(string title, string author, string duration)
+        {
+            _title = title;
+            _author = author;
+            _seconds = duration;
+        }
+
+    public void Display(){
+        Console.WriteLine($"Title: {_title} \nAuthor: {_author}\nDuration: {_seconds}\nThere are {_comments.Count} comments on this video. Here are the comments:\n");
+        foreach (Comment c in _comments){
+            c.Display();
+        }
+    }
 }
