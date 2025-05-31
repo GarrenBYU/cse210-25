3845bd44269d74e902ca326ba8f3f85215ccda47
GarrenBYU <mei21004@byui.edu>
2025-05-31 01:07:31 -0600

    Added a Collections class. Made the other objects and added the screenshot (final)

--- week04/YouTubeVideos/Collection.cs
+++ week04/YouTubeVideos/Collection.cs
@@ -6,6 +6,7 @@ class Collection
 
 
     public void Display(){
+        Console.Clear();
         foreach (Video v in _videos){
             v.Display();
         }
--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -16,6 +16,34 @@ class Program
         video1._comments.Add(comment3);
         video1._comments.Add(comment4);
         collection1._videos.Add(video1);
+
+
+        Video video2 = new Video("How to Survive the Apocolypse", "Kaladin", 7690);
+        Comment comment5 = new Comment("Shallan", "I think he needed better boots.");
+        Comment comment6 = new Comment("Dalinar", "Well taught with honor.");
+        Comment comment7 = new Comment("Adolin", "Bridge boy is good with a weapon.");
+        Comment comment8 = new Comment("Taravangian", "This was exceedingly dumb.");
+
+        video2._comments.Add(comment5);
+        video2._comments.Add(comment6);
+        video2._comments.Add(comment7);
+        video2._comments.Add(comment8);
+        collection1._videos.Add(video2);
+
+
+        Video video3 = new Video("How to Study", "Jasnah", 77777);
+        Comment comment9 = new Comment("Navani", "Good input with solid study ethics behind it.");
+        Comment comment10 = new Comment("Teft", "I think I understood every 15th word. Are you even talking in my own language?");
+        Comment comment11 = new Comment("Amaram", "Such a good presentation fit for a Bride.");
+        Comment comment12 = new Comment("Lopen", "The Lopen thinks this was a good presentation, but had too many arms. There are a ton of good jokes about a one armed Herdazian.");
+
+        video3._comments.Add(comment9);
+        video3._comments.Add(comment10);
+        video3._comments.Add(comment11);
+        video3._comments.Add(comment12);
+        collection1._videos.Add(video3);
+
+
         collection1.Display();
     }
 }
