19874c75b3f7acc51ce581f45479105a1bad6826
GarrenBYU <mei21004@byui.edu>
2025-06-01 15:44:38 -0600

    Took me a bit to understand, but I am getting things started

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,31 @@
+using System;
+
+public class Activity
+{
+    private DateTime _date;
+    private int _duration;
+    protected int _distance;
+    protected int _speed;
+    protected int _pace;
+
+    public Activity(DateTime date, int duration)
+    {
+        _date = date;
+        _duation = duration;
+    }
+
+    public abstract void distance();
+    public virtual void speed();
+    // {
+    //     _speed = (_distance / minutes) * 60;
+    // }
+    public abstract void pace();
+    // {
+    //     _pace = 60 / speed;
+    // }
+
+    public virtual string GetSummary()
+    {
+        return $"{_date} {_activity} ({_duration} min): Distance {_distance}, Speed {_speed} mph, Pace: {_pace} per mile"
+    }
+}
---
+++ week07/ExerciseTracking/Running.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Running : Activity
+{
+    public Running(DateTime date, int duration, int distance) : (date, duration)
+    {
+        _distance = distance;
+    }
+    
+}
