0f308836517a471021059b8c15200e5267cfa0d6
GarrenBYU <mei21004@byui.edu>
2025-05-31 23:49:01 -0600

    final

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,13 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
+        List<Shape> shapes = new List<Shape>();
         Square s1 = new Square("Green", 5);
-        Console.WriteLine($"The {s1.GetColor()} shape has an area of {s1.GetArea()}.");
+        shapes.Add(s1);
+        //Console.WriteLine($"The {s1.GetColor()} shape has an area of {s1.GetArea()}.");
 
         Rectangle s2 = new Rectangle("Blue", 6, 4);
-        Console.WriteLine($"The {s2.GetColor()} shape has an area of {s2.GetArea()}.");
+        shapes.Add(s2);
+        // Console.WriteLine($"The {s2.GetColor()} shape has an area of {s2.GetArea()}.");
 
         Circle s3 = new Circle("Red", 8);
-        Console.WriteLine($"The {s3.GetColor()} shape has an area of {s3.GetArea()}.");
+        shapes.Add(s3);
+        // Console.WriteLine($"The {s3.GetColor()} shape has an area of {s3.GetArea()}.");
+
+        foreach(Shape s in shapes)
+        {
+            Console.WriteLine($"The {s.GetColor()} shape has an area of {s.GetArea()}.");
+        }
     }
 }
