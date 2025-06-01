08038caa8346b0e3e0bfc12afbfeb1bd6bb676f1
GarrenBYU <mei21004@byui.edu>
2025-06-01 00:15:19 -0600

    made the goal class

---
+++ week06/EternalQuest/Goal.cs
@@ -0,0 +1,32 @@
+using System;
+
+public class Goal
+{
+    private string _shortName;
+    private string _description;
+    private string _points;
+
+    public Goal(string name, string description, string points)
+    {
+        _shortName = name;
+        _description = description;
+        _points = points;
+    }
+    public abstract void RecordEvent();
+    
+    public abstract bool IsComplete();
+
+    public string GetDetailsString()
+    {
+        if(IsComplete() = true)
+        {
+            return $"[x]{_shortName} ({_description})";
+        }
+        else
+        {
+            return $"[]{_shortName} ({_description})";
+        }
+    }
+
+    public abstract string GetStringRepresentation();
+}
